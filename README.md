c0d8eb670c265c13d2ab25305066133aaea07724
Huerte <huertejerald@gmail.com>
2025-09-30 20:36:20 +0800

    Nothing new

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GitGo
 An automation tools use to achieve ease in using git. This is a solution to our problem.
+
+## On Windows Setup
+
+### Getting the resources
+- download the repo first
+    `git clone https://github.com/Huerte/GitGo.git`
+- after downloading, go to the downloaded repo
+    `cd GitGo`
--- gitgo/gitgo.bat
+++
@@ -1,8 +0,0 @@
-:: this @echo off prevents the command window from showing up
-:: by add @ at the beginning of the line the command itself is not printed
-@echo off
-
-:: %~dp0 stands for "drive and path of this script"
-:: %* stands for "all the arguments passed to this script"
-:: so this line runs git_auto.py located in the same folder as this .bat file
-py "%~dp0gitgo.py" %*
